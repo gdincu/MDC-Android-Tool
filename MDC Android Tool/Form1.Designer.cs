3419417d6829bbb6fd8fd888ac1a69dfa1f6d5cd
NRO\I.G.DINCU <ionut.dincu@netrom.ro>
2021-05-07 11:58:29 +0300

    20210507 - Minor UI updates

--- MDC Android Tool/Form1.Designer.cs
+++ MDC Android Tool/Form1.Designer.cs
@@ -74,14 +74,18 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Location = new System.Drawing.Point(22, 12);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.HotTrack = true;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 3;
-            this.tabControl1.Size = new System.Drawing.Size(755, 388);
+            this.tabControl1.Size = new System.Drawing.Size(799, 388);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
             this.tabControl1.TabIndex = 2;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
@@ -89,10 +93,10 @@
             // 
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.listBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(747, 360);
+            this.tabPage1.Size = new System.Drawing.Size(791, 357);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Item Scan";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -119,10 +123,10 @@
             // 
             this.tabPage2.Controls.Add(this.button5);
             this.tabPage2.Controls.Add(this.button4);
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(747, 360);
+            this.tabPage2.Size = new System.Drawing.Size(791, 357);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Logs";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -154,10 +158,10 @@
             this.tabPage3.Controls.Add(this.button2);
             this.tabPage3.Controls.Add(this.listBox2);
             this.tabPage3.Controls.Add(this.button3);
-            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Location = new System.Drawing.Point(4, 27);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(747, 360);
+            this.tabPage3.Size = new System.Drawing.Size(791, 357);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "TripStart & EOT";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -272,10 +276,10 @@
             // 
             this.tabPage4.Controls.Add(this.button9);
             this.tabPage4.Controls.Add(this.button6);
-            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Location = new System.Drawing.Point(4, 27);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(747, 360);
+            this.tabPage4.Size = new System.Drawing.Size(791, 357);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Device";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -306,7 +310,7 @@
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.ClientSize = new System.Drawing.Size(799, 406);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
