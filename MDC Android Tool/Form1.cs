1a99da9f69caff37462d9b7f8962f8afa7ff14f7
gdincu <dici0137@scs.ubbcluj.ro>
2021-05-09 22:38:47 +0300

    20210509 - Updated the ItemAddControl

--- MDC Android Tool/Form1.cs
+++ MDC Android Tool/Form1.cs
@@ -9,14 +9,33 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using MDC_Android_Tool;
+using System.IO;
+using System.Xml.Linq;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        //Dictionaries used to store values for Items, Commands, URIs etc.
+        public IDictionary<string, string> Items = new Dictionary<string, string>();
+        public IDictionary<string, string> Commands = new Dictionary<string, string>();
+        public IDictionary<string, string> EOTBarcodes = new Dictionary<string, string>();
+        public IDictionary<string, string> URIs = new Dictionary<string, string>();
+        String Settings = Path.Combine(Environment.CurrentDirectory, "MDCAndroidTool.xml");
         public Form1()
         {
+            ReadValues(Items, "Items");
+            ReadValues(Commands, "Commands");
+            ReadValues(EOTBarcodes, "EOTBarcodes");
+            ReadValues(URIs, "URIs");
             InitializeComponent();
+        }
+        private void ReadValues(IDictionary<string, string> Dictionary, string TagName)
+        {
+            Dictionary.Clear();
+            foreach (XElement level1Element in XElement.Load(Settings).Elements(TagName))
+                foreach (XElement level2Element in level1Element.Elements(TagName[..^1]))
+                    Dictionary.Add(level2Element.Attribute("name").Value, level2Element.Attribute("value").Value);
         }
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
