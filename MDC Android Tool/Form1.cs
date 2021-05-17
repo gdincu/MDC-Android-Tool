eee09c84c2ccf5f4e072c02e759ac27c87c57f25
gdincu <dici0137@scs.ubbcluj.ro>
2021-05-17 22:06:22 +0300

    20210517 - Updated methods to be asynchronous

--- MDC Android Tool/Form1.cs
+++ MDC Android Tool/Form1.cs
@@ -34,12 +34,13 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        //https://docs.microsoft.com/en-us/archive/msdn-magazine/2013/march/async-await-best-practices-in-asynchronous-programming
         private async void Form1_Load(object sender, EventArgs e)
         {
-            ReadValues(Commands, "Commands");
+            await ReadValues(Commands, "Commands");
 
             // CheckHowManyDevicesAreCurrentlyConnected            
-            int nrOfDevices = await numberOfDevicesConnected();
+            int nrOfDevices = await NumberOfDevicesConnected();
 
             while (nrOfDevices != 3)
             {
@@ -56,29 +57,33 @@ namespace WindowsFormsApp1
                     break;
 
                 //Updates the nrOfDevices value
-                nrOfDevices = await numberOfDevicesConnected();
+                nrOfDevices = await NumberOfDevicesConnected();
             }
 
             ////Workaround - to be investigated
-            //runCommand("");
+            //RunCommand("");
 
-            ReadValues(Items, "Items");
-            ReadValues(EOTBarcodes, "EOTBarcodes");
-            ReadValues(URIs, "URIs");
-            ReadValues(HandheldDevices, "HandheldDevices");
+            await ReadValues(Items, "Items");
+            await ReadValues(EOTBarcodes, "EOTBarcodes");
+            await ReadValues(URIs, "URIs");
+            await ReadValues(HandheldDevices, "HandheldDevices");
 
             Items.ToList().ForEach(x => this.listBox1.Items.Add(x.Key));
             EOTBarcodes.ToList().ForEach(x => this.listBox2.Items.Add(x.Key));
             URIs.ToList().ForEach(x => this.listBox3.Items.Add(x.Key));
-            Commands.ToList().ForEach(x => this.listBox4.Items.Add(x.Key));  
+            Commands.ToList().ForEach(x => this.listBox4.Items.Add(x.Key));
+
         }
 
-        private void ReadValues (IDictionary<string, string> Dictionary, string TagName)
+        private async Task ReadValues (IDictionary<string, string> Dictionary, string TagName)
         {
             //https://docs.microsoft.com/en-us/dotnet/standard/linq/retrieve-value-element
             foreach (XElement level1Element in XElement.Load(Settings).Elements(TagName))
                 foreach (XElement level2Element in level1Element.Elements(TagName[..^1]))
                     Dictionary.Add(level2Element.Attribute("name").Value, level2Element.Value);
+
+            // Do asynchronous work.
+            await Task.Delay(100);
 
             //List<XElement> XMLSettings = XElement.Load(Settings).Elements(TagName).ToList();
             //XMLSettings.ForEach(x => 
@@ -88,11 +93,13 @@ namespace WindowsFormsApp1
 
         private async Task<String> scanItem(String ItemBarcode)
         {
+            // Do asynchronous work.
+            await Task.Delay(100);
             return Commands["ScanItem1"] + ItemBarcode + Commands["ScanItem2"];
         }
 
         //Used to run cmd commands
-        private async void runCommand(String command)
+        private async Task RunCommand(String command)
         {
             //https://stackoverflow.com/questions/19257041/run-cmd-command-without-displaying-it
             myProcess.StartInfo.FileName = "CMD.exe";
@@ -100,23 +107,29 @@ namespace WindowsFormsApp1
             myProcess.StartInfo.RedirectStandardOutput = true;
             myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             myProcess.Start();
+            
+            // Do asynchronous work.
+            await Task.Delay(100);
             //myProcess.WaitForExit();
         }
 
         //Used to run an external cmd script
-        private async void startProcess(String filename)
+        private async Task StartProcess(String filename)
         {
             myProcess.StartInfo.UseShellExecute = false;
             myProcess.StartInfo.CreateNoWindow = true;
             myProcess.StartInfo.FileName = Path.Combine(Environment.CurrentDirectory, filename);
             myProcess.StartInfo.RedirectStandardOutput = true;
             myProcess.Start();
+
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
         //Returns the number of connected devices
-        private async Task<int> numberOfDevicesConnected()
+        private async Task<int> NumberOfDevicesConnected()
         {
-            runCommand(Commands["NrOfDevices"]);
+            await RunCommand(Commands["NrOfDevices"]);
             int res = Int32.Parse(myProcess.StandardOutput.ReadToEnd());
             myProcess.Close();
             return res;
@@ -126,26 +139,28 @@ namespace WindowsFormsApp1
         {
 
             if (Items.ContainsKey(listBox1.SelectedItem.ToString()))
-                runCommand(scanItem(Items[listBox1.SelectedItem.ToString()]).Result);
+                await RunCommand(scanItem(Items[listBox1.SelectedItem.ToString()]).Result);
             else
                 MessageBox.Show("Item not found!");
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private async void button3_Click(object sender, EventArgs e)
         {
-            runCommand(Commands["Undocked"]);
+            await RunCommand(Commands["Undocked"]);
+            
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private async void button4_Click(object sender, EventArgs e)
         {
-            runCommand(Commands["ClearLogcat"]);
+            await RunCommand(Commands["ClearLogcat"]);
             MessageBox.Show("Cleared!", "Clear logcat");
+           
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private async void button5_Click(object sender, EventArgs e)
         {
-            startProcess("DeviceDetails.bat");
+            await StartProcess("DeviceDetails.bat");
             String DeviceDetails = myProcess.StandardOutput.ReadToEnd();
             myProcess.Close();
 
@@ -170,51 +185,58 @@ namespace WindowsFormsApp1
                 //Checks whether the folder already exists and creates it if needed
                 System.IO.Directory.CreateDirectory(Path.GetDirectoryName(saveFileDialog1.FileName) + DeviceDetails.Trim());
                 //Saves the entire myscan40 folder to the above folder
-                runCommand(Commands["PullFolder"] + Path.GetDirectoryName(saveFileDialog1.FileName) + DeviceDetails.Trim());
+                await RunCommand(Commands["PullFolder"] + Path.GetDirectoryName(saveFileDialog1.FileName) + DeviceDetails.Trim());
             }
+
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        private async void groupBox1_Enter(object sender, EventArgs e)
         {
-
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
+        private async void tabPage1_Click(object sender, EventArgs e)
         {
-
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        private async void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private async void button6_Click(object sender, EventArgs e)
         {
-            runCommand(Commands["Reboot"]);
+            await RunCommand(Commands["Reboot"]);
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private async void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-         
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
         {
-            runCommand(scanItem(EOTBarcodes[listBox2.SelectedItem.ToString()]).Result);
+            await RunCommand(scanItem(EOTBarcodes[listBox2.SelectedItem.ToString()]).Result);
         }
 
-        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private async void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void groupBox1_Enter_1(object sender, EventArgs e)
+        private async void groupBox1_Enter_1(object sender, EventArgs e)
         {
-
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private async void button7_Click(object sender, EventArgs e)
         {
             String _intent = Commands["Intent1"]
                 + textBox2.Text
@@ -223,39 +245,40 @@ namespace WindowsFormsApp1
                 + Commands["Intent3"]
                 + textBox3.Text+ "'";
 
-            runCommand(_intent);
+            await RunCommand(_intent);
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private async void button8_Click(object sender, EventArgs e)
         {
             //System.Diagnostics.Process.Start(Path.Combine(Environment.CurrentDirectory, "CloseCurrentApp.bat"));
-            startProcess("CloseCurrentApp.bat");
+            await StartProcess("CloseCurrentApp.bat");
             myProcess.Close();
 
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private async void button9_Click(object sender, EventArgs e)
         {
-            startProcess("DeviceDetails.bat");
+            await StartProcess("DeviceDetails.bat");
             String DeviceDetails = myProcess.StandardOutput.ReadToEnd();
             myProcess.Close();
             MessageBox.Show(DeviceDetails,"Paste these details where needed!");
             Clipboard.SetText(DeviceDetails);
         }
 
-        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        private async void listBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            // Do asynchronous work.
+            await Task.Delay(100);
         }
 
-        private void button10_Click(object sender, EventArgs e)
+        private async void button10_Click(object sender, EventArgs e)
         {
-            runCommand(Commands[listBox4.SelectedItem.ToString()]);
+            await RunCommand (Commands[listBox4.SelectedItem.ToString()]);
         }
 
-        private void button11_Click(object sender, EventArgs e)
+        private async void button11_Click(object sender, EventArgs e)
         {
-            startProcess("DeviceDetails.bat");
+            await StartProcess("DeviceDetails.bat");
             String DeviceDetails = myProcess.StandardOutput.ReadToEnd();
             myProcess.Close();
 
@@ -272,7 +295,7 @@ namespace WindowsFormsApp1
             saveFileDialog1.FileName = DeviceDetails.Trim() + "_";
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                 //https://stackoverflow.com/questions/27766712/using-adb-to-capture-the-screen
-                runCommand(Commands["ScreenCap"] + saveFileDialog1.FileName + ".png");
+                await RunCommand (Commands["ScreenCap"] + saveFileDialog1.FileName + ".png");
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -280,13 +303,13 @@ namespace WindowsFormsApp1
             SaveMyScan40Folder = checkBox1.Checked;
         }
 
-        private void button12_Click(object sender, EventArgs e)
+        private async void button12_Click(object sender, EventArgs e)
         {
             //Close the current app
-            startProcess("CloseCurrentApp.bat");
+            await StartProcess ("CloseCurrentApp.bat");
             myProcess.Close();
 
-            runCommand(Commands["GetCurrentApp"]);
+            await RunCommand (Commands["GetCurrentApp"]);
             String AppDetails = myProcess.StandardOutput.ReadToEnd();
             //https://www.c-sharpcorner.com/article/c-sharp-regex-examples/
             //Might need replacing
@@ -295,24 +318,24 @@ namespace WindowsFormsApp1
             //Wait 2 seconds
             System.Threading.Thread.Sleep(2000);
             //The below is a short version of adb shell monkey -p com.package.name -c android.intent.category.LAUNCHER 1
-            runCommand(Commands["StartApp"] + PackageName + " 1");
+            await RunCommand (Commands["StartApp"] + PackageName + " 1");
             myProcess.Close();
 
         }
 
-        private void button13_Click(object sender, EventArgs e)
+        private async void button13_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Is the device connected via USB?", "Device connected?", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
                 //Tcpip 5555 command
-                runCommand(Commands["Tcpip"]);
+                await RunCommand (Commands["Tcpip"]);
                 myProcess.Close();
 
                 //Wait 1 second -- needs to be here as otherwise the StandardOutput.ReadToEnd() functionality does not seem to work
                 System.Threading.Thread.Sleep(1000);
 
                 //IP route command
-                runCommand(Commands["Ip"]);
+                await RunCommand (Commands["Ip"]);
                 string[] tempResult = myProcess.StandardOutput.ReadToEnd().Split(" ");
                 string IpAddress = (tempResult.Length >= 11) ? tempResult[11] : tempResult.ToString();
                 MessageBox.Show(IpAddress);
@@ -320,14 +343,14 @@ namespace WindowsFormsApp1
 
                 if (MessageBox.Show("Please disconnect the device from the USB port and tap OK when ready!", "Disconnect the device!", MessageBoxButtons.OKCancel) == DialogResult.OK) {
                     //Disconnects all devices
-                    runCommand(Commands["Disconnect"]);
+                    await RunCommand (Commands["Disconnect"]);
                     myProcess.Close();
                     
                     //Wait 1 second 
                     System.Threading.Thread.Sleep(1000);
 
                     //Connects over IP to the selected device
-                    runCommand(Commands["Connect"] + IpAddress);
+                    await RunCommand (Commands["Connect"] + IpAddress);
                     MessageBox.Show("Connected to "+ IpAddress);
                     myProcess.Close();
                 }
